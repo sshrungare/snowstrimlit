0c7553e60e45d3077df59972153f7bb8f1e1973c
Swapnil <31124947+sshrungare@users.noreply.github.com>
2023-02-10 17:41:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas 
 my_fruit = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit.set_index('Fruit')
+my_fruit = my_fruit.set_index('Fruit')
 streamlit.multiselect("Pick Some fruits:" , list(my_fruit.index))
 
 streamlit.dataframe(my_fruit)
