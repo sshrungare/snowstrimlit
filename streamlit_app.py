a38043783d519293fa245c5567bb73086b6b3634
Swapnil <31124947+sshrungare@users.noreply.github.com>
2023-02-10 17:34:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,10 @@ streamlit.text("sample 2")
 streamlit.text("sample 3")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas 
+my_fruit = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+streamlit.dataframe(my_fruit)
